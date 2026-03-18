a1dabc4c440dd489f966c537ffb0433aa942efd8
Quinton Khuwiseb <khuwisebquinton@gmail.com>
2026-03-18 13:19:21 +0200

    codes front end to link to api

---
+++ creatures-API/script.js
@@ -0,0 +1,45 @@
+const API = "http://localhost:3000/creatures";
+
+async function addCreature() {
+    const name = document.getElementById("name").ariaValueMax;
+    const power = document.getElementById("power").ariaValueMax;
+    const dangerLevel = document.getElementById("danger").ariaValueMax;
+
+    await fetch(API, {
+        method: "POST",
+        headers: { "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ name, power, dangerLevel })
+    });
+
+    getCreatures();
+}
+
+async function getCreatures() {
+    const res = await fetch(API);
+    const data = await res.json();
+
+    const list = document.getElementById("list");
+    list.innerHTML = "";
+
+    data.forEach(c => {
+        list.innerHTML += `
+        <div class="creature">
+        <strong>${c.name}</strong>
+        <br>
+        Power: ${c.power}
+        <br>
+        Danger: ${c.dangerLevel}<br>
+        ID: ${c.id}
+        <br>
+        <button onclick="deleteCreature(${c.id})">Delete</button>
+        </div>`;
+    });  
+}
+
+async function deleteCreature(id) {
+    await fetch(API + "/"+ id, {
+        method: "DELETE"
+    });
+    getCreatures();
+}
